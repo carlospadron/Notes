315775f447f5dcbf1a96f62c59252853f61628bc
carlos <padron.ca@gmail.com>
2025-05-26 18:10:08 +0100

    add list of packages to install

---
+++ packages/paquetes_conda.sh
@@ -0,0 +1,3 @@
+conda config --add channels conda-forge
+conda update --all
+conda install pandas geopandas psycopg2 scikit-learn geoalchemy2 pysal networkx plotly pyspark statsmodels polars Fiona networkx
